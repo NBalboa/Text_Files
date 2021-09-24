3e0b169341d9e40c0030d632fbac8e29c3fe39bd
Nbalboa <nickojek2x@gmail.com>
2021-09-24 19:16:14 +0800

    Laboratory - text file

---
+++ TextFile/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ShowUI();
+            bool isContinue = true;
+            int select;
+
+            while (isContinue)
+            {
+                select = StudentData.GetInt("Select Option: ");
+                if (select == 1)
+                {
+                    GetStudentInfoFromUi();
+                }
+                else if (select == 2)
+                {
+                    DisplayStudent();
+                }
+                else if (select == 3)
+                {
+                    isContinue = false;
+                }
+            }
+
+            Console.ReadLine();
+        }
+
+        private static void DisplayStudent()
+        {
+            StudentData.ShowStudent();
+        }
+        private static void GetStudentInfoFromUi()
+        {
+            bool isContinue = true;
+
+            while (isContinue)
+            {
+                StudentData.AddStudent();
+                string answer = StudentData.GetString("Add Student? (Y/N): ");
+
+                if (answer.ToLower() == "y")
+                {
+                    
+                    isContinue = true;
+                }
+                else if (answer.ToLower() == "n")
+                {
+                    isContinue = false;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid Input. Please enter y or n only");
+                }
+            }
+        }
+      
+        private static void ShowUI()
+        {
+            Console.WriteLine("1. Add Student: ");
+            Console.WriteLine("2. Show Student: ");
+            Console.WriteLine("3. Exit: ");
+        }
+    }
+}
---
+++ TextFile/StudentData.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TextFile.Model;
+
+namespace TextFile
+{
+    public static class StudentData
+    {
+        static List<StudentModel> students = new List<StudentModel>();
+        static StudentModel student = new StudentModel();
+
+
+        public static void AddStudent()
+        {
+            string firstName, lastName, address;
+            int age;
+
+            firstName = GetString("Enter Student First name: ");
+            lastName = GetString("Enter Student Last name: ");
+            age = GetInt("Enter a Student age: ");
+            address = GetString("Enter Student address: ");
+
+            student.FirstName = firstName;
+            student.LastName = lastName;
+            student.Age = age;
+            student.Address = address;
+
+            students.Add(student);
+            
+
+            using (StreamWriter sw = new StreamWriter("student.txt"))
+            {
+                foreach (var studentModel in students)
+                {
+                    sw.WriteLine($"{studentModel.FirstName}, {studentModel.LastName}, {studentModel.Age}, {studentModel.Address}");
+                }
+            }
+            using (StreamReader sr = new StreamReader("student.txt"))
+            {
+                string ageString = student.Age.ToString();
+
+                student.FirstName = sr.ReadLine();
+                student.LastName = sr.ReadLine();
+                ageString = sr.ReadLine();
+                student.Address = sr.ReadLine();
+            }
+
+        }
+
+        public static void ShowStudent()
+        {
+            using (StreamReader rd = new StreamReader("student.txt"))
+            {
+                string line = rd.ReadLine();
+                string[] studentInfo = line.Split(',');
+                foreach (var student in studentInfo)
+                {
+                    Console.WriteLine($"First name : {student[0]} Last name: {student[1]} Age : {student[2]} Address: {student[3]}");
+                }
+            }
+        }
+
+        public static string GetString(string message)
+        {
+            string output = "";
+            while (string.IsNullOrEmpty(output))
+            {
+                Console.Write(message);
+                output = Console.ReadLine();
+            }
+
+            return output;
+        }
+
+        public static int GetInt(string message)
+        {
+            string numberText = GetString(message);
+            int number;
+            bool isValidNumber = int.TryParse(numberText, out number);
+
+            while (!isValidNumber)
+            {
+                Console.WriteLine("Invalid input. Please enter a digit");
+                numberText = GetString(message);
+                isValidNumber = int.TryParse(numberText, out number);
+            }
+
+            return number;
+        }
+    }
+}
